61a9f2b4da9abe002c1f69f2e64cbee00ed45c81
TheBlankness <iddinishak@gmail.com>
2025-01-09 12:17:57 +0800

    t

--- src/routes/auth/confirm-reset/+server.ts
+++ src/routes/auth/confirm-reset/+server.ts
@@ -1,31 +1,22 @@
-import type { EmailOtpType } from '@supabase/supabase-js'
-import { redirect } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit';
 
-import type { RequestHandler } from './$types'
+import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
-  const token_hash = url.searchParams.get('token_hash')
-  const type = url.searchParams.get('type') as EmailOtpType | null
-  const next = url.searchParams.get('next') ?? '/'
+	const code = url.searchParams.get('code') as string;
 
-  /**
-   * Clean up the redirect URL by deleting the Auth flow parameters.
-   *
-   * `next` is preserved for now, because it's needed in the error case.
-   */
-  const redirectTo = new URL(url)
-  redirectTo.pathname = next
-  redirectTo.searchParams.delete('token_hash')
-  redirectTo.searchParams.delete('type')
+	/**
+	 * Clean up the redirect URL by deleting the Auth flow parameters.
+	 *
+	 * `next` is preserved for now, because it's needed in the error case.
+	 */
 
-  if (token_hash && type) {
-    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
-    if (!error) {
-      redirectTo.searchParams.delete('next')
-      redirect(303, '/private/profile')
-    }
-  }
+	if (code) {
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+		if (!error) {
+			redirect(303, '/private/profile');
+		}
+	}
 
-  redirectTo.pathname = '/auth/error'
-  redirect(303, redirectTo)
-}
+	redirect(303, '/auth/error');
+};
