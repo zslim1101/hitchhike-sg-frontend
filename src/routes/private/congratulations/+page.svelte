be858b6cc8e43674f24fc6d5ae4f9b7a5be58729
TheBlankness <iddinishak@gmail.com>
2025-01-16 01:02:01 +0800

    now logs in when link click

--- src/routes/private/congratulations/+page.svelte
+++ src/routes/private/congratulations/+page.svelte
@@ -1,10 +1,8 @@
 <div class="flex flex-col items-center">
 	<h1 class="mb-4 text-3xl font-semibold">Congratulations!</h1>
-	<p class="mb-8 text-lg">
-		Your Email has been verified. Now you can now login and create your first trip.
-	</p>
+	<p class="mb-8 text-lg">Your Email has been verified. Now you can now create your first trip.</p>
 	<a
-		href="/auth/login"
+		href="/private/trips"
 		class="rounded bg-secondary-500 px-6 py-3 text-white transition hover:bg-secondary-600"
 		>Go and create your first trip</a
 	>
